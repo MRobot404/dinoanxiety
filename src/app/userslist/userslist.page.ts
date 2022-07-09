bbcf791128c58636866fe02bbc4e0dbef5cf7c70
Oscar Escobar <qomc16@gmail.com>
2022-07-08 23:45:28 -0600

    paso de id unicamente

--- src/app/userslist/userslist.page.ts
+++ src/app/userslist/userslist.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-userslist',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userslist.page.scss'],
 })
 export class UserslistPage implements OnInit {
+  @Input() dataEntrante: any;
+  dinos = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.http
+      .get<any>('http://demo2854538.mockable.io/dino')
+      .subscribe((res) => {
+        this.dinos = res;
+        console.log('Entrando data ' + this.dataEntrante);
+        let valorBuscado = 1;
+        const resultado = this.dinos.find((dino) => dino.id === valorBuscado);
+        console.log(resultado);
+      });
   }
-
+  hola(id){
+    console.log(id)
+  }
 }
